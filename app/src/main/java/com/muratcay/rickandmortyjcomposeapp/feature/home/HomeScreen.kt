97838ed6d890c63afef0bb9ae6f67370ca75a325
muratcay <muratthebestof@gmail.com>
2024-11-09 15:02:15 +0300

    initial commit

---
+++ app/src/main/java/com/muratcay/rickandmortyjcomposeapp/feature/home/HomeScreen.kt
@@ -0,0 +1,2 @@
+package com.muratcay.rickandmortyjcomposeapp.feature.home
+
---
+++ app/src/main/java/com/muratcay/rickandmortyjcomposeapp/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.muratcay.rickandmortyjcomposeapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val ColorDarkGray = Color(0xff262d3a) // Dark Gray
+val ColorLightBlue = Color(0xffb5eefd) // Light Blue
+val ColorLightBeige = Color(0xffead7ce) // Light Beige
+val ColorWhite = Color(0xffffffff) // White
+val ColorLightBeigeText = Color(0xffead7ce) // Light Beige Text
